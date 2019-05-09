7cd3f65be7587a696505fca8414069ebc6756aa3
Lord of Khaos <liam@billandmary.com>
2019-05-08 22:20:49 -0400

    Update Utilities.cs

--- MiniMode/Utilities.cs
+++ MiniMode/Utilities.cs
@@ -1,8 +1,11 @@
+using Smod2.API;
+using SmodTeam = Smod2.API.Team;
+
 namespace MiniMode
 {
 	public class Utilities
 	{
-		private MiniModePlugin _plugin;
+		private readonly MiniModePlugin _plugin;
 		
 		public Utilities(MiniModePlugin plugin)
 		{
@@ -11,8 +14,14 @@ namespace MiniMode
 		
 		public void CheckForEscapees()
 		{
+			foreach (Player player in this._plugin.Server.GetPlayers())
+			{
+				float yAxisLocation = player.GetPosition().y;
+				if (yAxisLocation > 900 && player.TeamRole.Team == (SmodTeam.CLASSD | SmodTeam.SCIENTIST)) // or whatever -> in LCZ
+					player.Teleport(player.TeamRole.Team == SmodTeam.CLASSD ? new Vector(0, 0, 0) : new Vector(1, 0, 0)); // placeholder
+			}
+			
 			throw new System.NotImplementedException();
 		}
-		
 	}
 }
